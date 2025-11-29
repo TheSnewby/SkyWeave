2b95ce663ebe5c5740aaffc7eca51a7b914c558c
TheSnewby <stephen.j.newby@gmail.com>
2025-11-29 15:56:33 -0600

    added swarm behaviour control attributes to the swarm_coord class, as well as  setters and getters

--- sim/src/swarm_coordinator.h
+++ sim/src/swarm_coordinator.h
@@ -7,15 +7,33 @@ class SwarmCoordinator {
 private:
 	UAVSimulator& sim;
 	std::mutex coord_mutex;
+	// cohesion sliders
+	double cohesion;
+	double separation;
+	double alignment;
+	double max_speed;
+	double target_altitude;
 
 public:
-	SwarmCoordinator(UAVSimulator s, formation num = RANDOM) : sim(s) {};
+	SwarmCoordinator(UAVSimulator s, formation num = RANDOM) : sim(s) {
+		cohesion = separation = alignment = 1.00;
+		double max_speed = 30;
+		double target_altitude = 150;
+	};
 
 	// getters
 	UAVSimulator& get_sim() { return sim; }
-
+	double get_cohesion() { return cohesion; }
+	double get_separation() { return separation; }
+	double get_alignment() { return alignment; }
+	double get_max_speed() { return max_speed; }
+	double get_target_altitude() { return target_altitude;}
 
 	// updaters
-	
+	void set_cohesion(double coh) { cohesion = coh; }
+	void set_separation(double sep) { separation = sep; }
+	void set_alignment(double align) { alignment = align; }
+	void set_max_speed(double max) { max_speed = max; }
+	void set_target_altitude(double target) { target_altitude = target; }
 
 };
